142b6bd9d7b4fad6e160eb9120256b1c1f855892
Park-MinJe <phone7101@naver.com>
2023-08-25 17:51:16 +0900

    update indexOnProgToEpsg4326
    /*TODO*/
    Range of result is not properate

--- src/Program.Coordinate.cs
+++ src/Program.Coordinate.cs
@@ -245,20 +245,48 @@ namespace surveillance_system
             Point rt = new Point();
             double x = p.x;
             double y = p.y;
+            double G = 60 * 1.1515 * 1.609344 * 1000 * 1000 * (Math.PI / 180);
 
-            double x_proportion = x / x_size;
-            double y_proportion = y / y_size;
+            Point leftUpperCorner = new Point(lowerCorner.x, upperCorner.y, 0);
 
-            rt.setX(lowerCorner.x + (upperCorner.x - lowerCorner.x) * x_proportion);
-            rt.setY(upperCorner.y - (upperCorner.y - lowerCorner.y) * y_proportion);
+            rt.setX(
+                Math.Round(
+                    Math.Acos((Math.Cos(x / G) - Math.Pow(Math.Sin(leftUpperCorner.y * (Math.PI / 180)), 2))
+                     / Math.Pow(Math.Cos(leftUpperCorner.y * (Math.PI / 180)), 2)) * 180 / Math.PI + leftUpperCorner.x,
+                    7
+                    )
+                );
+            
+            double possibleLat1 = Math.Round(
+                y / G * 180 / Math.PI + leftUpperCorner.y,
+                7);
+            double possibleLat2 = Math.Round(
+                360 - y / G * 180 / Math.PI + leftUpperCorner.y,
+                7);
+
+            rt.setY(possibleLat1);
+            //Console.WriteLine("possibleLat1 = " + possibleLat1);
+            //Console.WriteLine("possibleLat2 = " + possibleLat2);
+            //if(possibleLat1 > lowerCorner.y && possibleLat1 < upperCorner.y)
+            //{
+            //    rt.setY(possibleLat1);
+            //}
+            //else rt.setY(possibleLat2);
 
             //debug
-            //rt.printString();
+            p.printString();
+            //lowerCorner.printString();
+            //upperCorner.printString();
+            leftUpperCorner.printString();
+            //Console.WriteLine("x radian = " + Math.Acos((Math.Cos(x / G) - Math.Pow(Math.Sin(leftUpperCorner.y * (Math.PI / 180)), 2))
+            //         / Math.Pow(Math.Cos(leftUpperCorner.y * (Math.PI / 180)), 2)) * 180 / Math.PI);
+            //Console.WriteLine("y radian = " + Math.Atan((Math.Cos(y / G) - Math.Cos(leftUpperCorner.y * (Math.PI / 180))) / Math.Sin(leftUpperCorner.y * (Math.PI / 180))));
+            rt.printString();
 
             return rt;
         }
 
-        public static double getDistanceBetweenPointsOfepsg4326(double lat0, double lon0, double lat1, double lon1)
+        public static double getDistanceBetweenPointsOfepsg4326(double lon0, double lat0, double lon1, double lat1)
         {
             double minlat = Math.Min(lat0, lat1);
             double maxlat = Math.Max(lat0, lat1);
